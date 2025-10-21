a69a47929b11cd8895ad8b5df6b4294d4ec335f9
Мария <mahadrum@yandex.ru>
2025-10-21 09:52:32 +0300

    Добавлен массив делегатов

--- ShopOnLineDeskTop/FormCatalog.cs
+++ ShopOnLineDeskTop/FormCatalog.cs
@@ -61,13 +61,12 @@ namespace ShopOnLineDeskTop
         private void comboBoxFilters_SelectedIndexChanged(object sender, EventArgs e)
         {
             List<Product> temp = new List<Product>();
-            FilerFunction filerFunctionAll = catalog.GetAll;
-            FilerFunction filerFunctionLowPrice = catalog.GetLowPrice;
+            FilerFunction[] filerFunctions = {catalog.GetAll, catalog.GetLowPrice };
 
             switch (comboBoxFilters.SelectedIndex)
             {
-                case 0: temp = filerFunctionAll(); break;
-                case 1: temp = filerFunctionLowPrice(); break;
+                case 0: temp = filerFunctions[0](); break;
+                case 1: temp = filerFunctions[1](); break;
             }
             showProducts(temp);
         }
